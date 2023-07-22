ea38210998bdce77b60449c535a08a99b44767d6
Ali-Fattahian <ali.fattahian1378@gmail.com>
2023-07-22 22:11:20 +0430

    add animated background to app (for the entire project), move HomePage.js to homepage folder

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/homepage/HomePage";
 import { Suspense } from "react";
 import Loader from "./components/loader/Loader";
 
 function App() {
   return (
-    <Router>
-      <Suspense fallback={<Loader />}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      </Suspense>
-    </Router>
+    <div id="app">
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+      <div id="title">
+        <span>Todo app</span>
+        <br></br>
+        <span>Tutorial</span>
+      </div>
+      <Router>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </Suspense>
+      </Router>
+    </div>
   );
 }
 
