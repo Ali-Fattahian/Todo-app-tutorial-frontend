543d74db91b492e6b9eb6968f4da678f41a75871
Ali-Fattahian <ali.fattahian1378@gmail.com>
2023-07-22 21:38:36 +0430

    add suspense as a wrapper around routes with Loader.js as fallback

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import { Suspense } from "react";
+import Loader from "./components/loader/Loader";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
