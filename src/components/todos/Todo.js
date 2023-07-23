54d6b8451ea0c888aa2c8cc54ffbd25345971e96
Ali-Fattahian <ali.fattahian1378@gmail.com>
2023-07-23 18:18:23 +0430

    create a fake todo and style it

--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -1,7 +1,18 @@
+import classes from "./Todos.module.css";
+
 const Todo = () => {
   return (
-    <div>Todo</div>
-  )
-}
+    <div className={classes.todo}>
+      <div className={classes["todo-title"]}>Learn React Router Dom</div>
+      <div className={classes["todo-lower"]}>
+        <small>23 / 7 / 2023</small>
+          <label htmlFor="isRead" className={classes['isRead']}>
+          <input type="checkbox" value={false} id="isRead" className={classes['checkbox']} />
+          Read
+        </label>
+      </div>
+    </div>
+  );
+};
 
-export default Todo
+export default Todo;
