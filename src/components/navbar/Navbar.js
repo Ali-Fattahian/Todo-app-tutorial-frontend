846b5d1f67295f6c2b62aaec9aad191eaa18bef3
Ali-Fattahian <ali.fattahian1378@gmail.com>
2023-07-25 22:11:37 +0430

    add logout functionality to navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,11 +1,20 @@
 import classes from "./Navbar.module.css";
 
 const Navbar = () => {
+  const logout = () => {
+    localStorage.removeItem("token");
+    window.location.reload()
+  };
+
   return (
     <nav id={classes["navbar-container"]}>
       <section id={classes["navbar"]}>
         <a href="/">Home</a>
-        <a href="/login">Login</a>
+        {localStorage.getItem("token") ? (
+          <p onClick={() => logout()}>Log out</p>
+        ) : (
+          <a href="/login">Log in</a>
+        )}
         <a href="/finished-todos">Finished</a>
         <a href="/unfinished-todos">Unfinished</a>
       </section>
